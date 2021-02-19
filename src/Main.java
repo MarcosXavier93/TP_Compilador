49b7dd7848ddee2ac6c6c44d5a540fe19f457a82
Marcos Xavier <marcos_mjsx@hotmail.com>
2021-02-19 10:25:56 -0300

    Al- Atualizando Main

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,31 @@
 
-Na etapa 1, o compilador deverá exibir a sequência de tokens identificados e os símbolos (identificadores e
+/*Na etapa 1, o compilador deverá exibir a sequência de tokens identificados e os símbolos (identificadores e
 palavras reservadas) instalados na Tabela de Símbolos. Nas etapas seguintes, isso não deverá ser exibido.
  
 Commit para reparar possíveis bugs
 
+*/
 import analisador_lexico.Lexer;
 import analisador_lexico.Tag;
 import analisador_lexico.Token;
 import exception.InvalidTokenException;
-
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Scanner;
 import java.util.ArrayList;
-
 public class Main {
-
-
 	public static void main(String[] args) {
-		ArrayListToken tokens = new ArrayListToken ();
+		ArrayList<Token> tokens = new ArrayList<Token> ();
 		Lexer L = null;
 		int line = -5;
 		try {
-		//	L = new Lexer(D- ParticularEstudosCefetOneDrive - aluno.cefetmg.br2020.2compiltrabTP_Compiladorsrccodigos_testeTeste1.txt);
-			L = new Lexer(codigos_testeTeste7.txt);
-			
-			L.adicionapalavras();Inicia adicionando palavras reservadas
-			System.out.println( Tokens lidos );
-			 Apenas para entrar no laço
+			//L = new Lexer("D:\\- Particular\\Estudos\\Cefet\\OneDrive - aluno.cefetmg.br\\2020.2\\compil\\trab\\TP_Compilador\\src\\codigos_teste\\Teste1.txt");
+			L = new Lexer("codigos_teste/Teste2.txt");
+			L = new Lexer("codigos_teste/Teste7.txt");
+
+			L.adicionapalavras();//Inicia adicionando palavras reservadas
+			System.out.println("**** Tokens lidos ****");
+			// Apenas para entrar no laço
 			Token T = new Token(0, line);
 			while (T.tag != Tag.EOF) {
 				try {
@@ -37,7 +35,7 @@ public class Main {
 					T.imprimeToken(T);
 					tokens.add(T);
 					line = T.line;
-				} catch (InvalidTokenException  IOException e) {
+				} catch (InvalidTokenException | IOException e) {
 					System.out.println(e.getMessage());
 					try {
 						L.readch();
